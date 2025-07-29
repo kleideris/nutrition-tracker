8df6207549a09938093bc918cb9cd4cd695f0662
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-29 21:19:13 +0300

    feat: add IUserRepository interface

---
+++ server - backend/NutritionTracker.Api/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using NutritionTracker.Api.Data;
+using NutritionTracker.Api.DTO;
+
+namespace NutritionTracker.Api.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> AuthenticateAsync(string username, int password);
+        Task<User?> UpdateUserAsync(int userId, User user);  // TODO: think if i should change User with a UserDTO)
+        Task<User?> UpdateUserProfileAsync(int userId, UserProfileDTO dto);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize,
+            List<Func<User, bool>> predicates);
+        // Task<bool> IsEmailTakenAsync(string email);
+    }
+}
