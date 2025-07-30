f325a1f8c515c0c261989d8b5debc726199d29e4
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-30 14:54:27 +0300

    feat: add UserRepository Implementation

---
+++ server - backend/NutritionTracker.Api/Repositories/UserRepository.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NutritionTracker.Api.Data;
+using NutritionTracker.Api.DTO;
+using NutritionTracker.Api.Security;
+using System.Linq.Expressions;
+
+namespace NutritionTracker.Api.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        private readonly IMapper _mapper;
+
+        public UserRepository(AppDBContext context) : base(context)
+        {
+        }
+
+        public async Task<User?> AuthenticateAsync(string usernameOrEmail, string password)
+        {
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Username == usernameOrEmail || u.Email == usernameOrEmail);
+
+            if (user == null || !EncryptionUtil.IsValidPassword(password, user.PasswordHash))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        // By using Expression<Func<User, bool>> Instead of Func<User, bool> we allows Entity Framework to parse and convert
+        // the filters into SQL queries. Otherwise because it cant translate the func delegates
+        // int sql queries, it would load everthing in memory and filter them there.
+        public async Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize,
+            List<Expression<Func<User, bool>>> predicates)
+        {
+            int skip = (pageNumber - 1) * pageSize;
+            IQueryable<User> query = context.Users;
+
+            if (predicates != null && predicates.Any())
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate);
+                }
+
+            }
+            query = query.Skip(skip).Take(pageSize);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<User?> GetUserByUsernameAsync(string username) => 
+            await context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+        public async Task<User?> UpdateUserAsync(int id, User user)
+        {
+            var existingUser = await context.Users.FindAsync(id);
+            if (existingUser == null)
+            {
+                return null;
+            }
+
+            context.Users.Attach(user);
+            context.Entry(user).State = EntityState.Modified;  // Marks the UserProfile entity as modified
+
+            return existingUser;
+        }
+
+        public async Task<User?> UpdateUserProfileAsync(int id, UserProfileDTO dto)
+        {
+            var existingUser = await context.Users
+                .Include(u => u.UserProfile)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (existingUser == null)
+            {
+                return null;
+            }
+
+            if (existingUser.UserProfile == null)
+            {
+                existingUser.UserProfile = new UserProfile();
+                context.UserProfiles.Add(existingUser.UserProfile);
+            }
+
+
+            
+            _mapper.Map(dto, existingUser.UserProfile);  // Usage of AutoMapper to update the UserProfile entity with DTO data
+            context.Entry(existingUser.UserProfile).State = EntityState.Modified;  // Marks the UserProfile entity as modified
+
+            return existingUser;
+        }
+    }
+}
