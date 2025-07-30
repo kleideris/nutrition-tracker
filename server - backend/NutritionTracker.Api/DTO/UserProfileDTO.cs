20ade623ee3b690fb78f07f4f2c3e658c2cbd0bf
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-30 14:59:00 +0300

    feat: add UserProfileDTO

---
+++ server - backend/NutritionTracker.Api/DTO/UserProfileDTO.cs
@@ -0,0 +1,13 @@
+﻿using NutritionTracker.Api.Core.Enums;
+
+namespace NutritionTracker.Api.DTO
+{
+    public class UserProfileDTO
+    {
+        public DateOnly DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public double HeightCm { get; set; }
+        public double WeightKg { get; set; }
+        public string? ActivityLvl { get; set; }
+    }
+}
