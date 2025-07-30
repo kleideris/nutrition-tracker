c00284f0c5d75b138a8082f91735ce6f13405621
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-30 22:50:08 +0300

    feat: add UserService with partial implementation

---
+++ server - backend/NutritionTracker.Api/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using NutritionTracker.Api.Core.Filters;
+using NutritionTracker.Api.Data;
+using NutritionTracker.Api.DTO;
+
+namespace NutritionTracker.Api.Services
+{
+    public interface IUserService
+    {
+        Task<User?> VerifyAndGetUserAsync(UserLoginDTO credentials);
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize, UserFiltersDTO userFilterDTO);
+
+        //string CreateUserToken(int userId, string username, string email, UserRole userRole, string appSecurityKey);
+    }
+}
---
+++ server - backend/NutritionTracker.Api/Services/UserService.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using NutritionTracker.Api.Core.Filters;
+using NutritionTracker.Api.Core.Helpers;
+using NutritionTracker.Api.Data;
+using NutritionTracker.Api.DTO;
+using NutritionTracker.Api.Exceptions;
+using NutritionTracker.Api.Repositories;
+using Serilog;
+using System;
+using System.Linq.Expressions;
+
+namespace NutritionTracker.Api.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UserService> _logger;
+
+        public UserService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<UserService> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = new LoggerFactory().AddSerilog().CreateLogger<UserService>();  // create the Logger for user service using a factory method
+        }
+
+        public async Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize, UserFiltersDTO userFilterDTO)
+        {
+            List<User> users;
+            Expression<Func<User, bool>> predicates;
+
+            try
+            {
+                predicates = UserPredicateBuilder.BuildPredicates(userFilterDTO);
+
+                users = await _unitOfWork.userRepository
+                    .GetAllUsersFilteredPaginatedAsync(pageNumber, pageSize, predicates);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("{Message},{Exception}", e.Message, e.StackTrace);
+                throw;
+            }
+            return users;
+        }
+
+        public Task<User?> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> VerifyAndGetUserAsync(UserLoginDTO credentials)
+        {
+            throw new NotImplementedException();
+        }                
+    }
+}
+
