baa1108c3f0a9d511e1e372a7f13f595308a498f
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-30 14:58:39 +0300

    feat: add automapper, implement UserProfile map to DTO

---
+++ server - backend/NutritionTracker.Api/Configuration/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using NutritionTracker.Api.Data;
+using NutritionTracker.Api.DTO;
+
+namespace NutritionTracker.Api.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<UserProfile, UserProfileDTO>();
+            CreateMap<UserProfileDTO, UserProfile>()
+                .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null)); // Ignores nulls to protect existing values.
+
+        }
+    }
+}
