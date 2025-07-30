d37bc61dbb338c0a7d077e379f252ae5e7fb09f7
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-30 14:55:56 +0300

    feat: add UnitOfWork with user repository inside

---
+++ server - backend/NutritionTracker.Api/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace NutritionTracker.Api.Repositories
+{
+    // Defines a contract for working with multiple repositories in a single transactional scope.
+    public interface IUnitOfWork
+    {
+        UserRepository userRepository { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
---
+++ server - backend/NutritionTracker.Api/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using NutritionTracker.Api.Data;
+
+namespace NutritionTracker.Api.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDBContext _context;
+
+        public UnitOfWork(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public UserRepository userRepository => new (_context);
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
