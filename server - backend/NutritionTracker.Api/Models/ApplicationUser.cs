ad2c841fd0cd5c70c8dfbbc13abc04ecbe9e5fb4
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-31 20:00:14 +0300

    feat: add application user model

---
+++ server - backend/NutritionTracker.Api/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿namespace NutritionTracker.Api.Models
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+    }
+}
