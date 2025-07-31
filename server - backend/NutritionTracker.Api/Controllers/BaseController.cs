7ba8395b473003015beae592c1a9d2039e7252a4
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-31 20:02:35 +0300

    feat: add base and user controller with some implementation

---
+++ server - backend/NutritionTracker.Api/Controllers/BaseController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NutritionTracker.Api.Models;
+using NutritionTracker.Api.Services;
+using System.Security.Claims;
+
+namespace NutritionTracker.Api.Controllers
+{
+    // [Produces("application/json")] default since it derives from ControllerBase
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public readonly IApplicationService _applicationService;
+
+        protected BaseController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        private ApplicationUser? _appUser;
+
+        protected ApplicationUser? AppUser
+        {
+            get
+            {
+                if (User != null && User.Claims != null && User.Claims.Any())
+                {
+
+                    var claimsTypes = User.Claims.Select(x => x.Type);
+                    if (!claimsTypes.Contains(ClaimTypes.NameIdentifier))
+                    {
+                        return null;
+                    }
+
+                    // User is an instance of ClaimsPrincipal
+                    var userClaimsId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                    _ = int.TryParse(userClaimsId, out int id);
+
+                    _appUser = new ApplicationUser
+                    {
+                        Id = id
+                    };
+
+                    var userClaimsName = User.FindFirst(ClaimTypes.Name)?.Value;
+
+                    _appUser.Username = userClaimsName!;
+                    _appUser.Email = User.FindFirst(ClaimTypes.Email)?.Value;
+                    return _appUser;
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ server - backend/NutritionTracker.Api/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NutritionTracker.Api.DTO;
+using NutritionTracker.Api.Exceptions;
+using NutritionTracker.Api.Services;
+
+namespace NutritionTracker.Api.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public UserController(IApplicationService applicationService, IConfiguration configuration,
+            IMapper mapper) : base(applicationService)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserReadOnlyDTO>> GetUserById(int id)
+        {
+            var user = await _applicationService.UserService.GetUserByIdAsync(id) ?? throw new EntityNotFoundException("User", "User: " + id + " NotFound");
+            var returnedDto = _mapper.Map<UserReadOnlyDTO>(user);
+            return Ok(returnedDto);
+        }
+    }
+}
