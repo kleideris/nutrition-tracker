aaf135b7e99aec863f58529e86b9b67b262ad02e
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-31 19:51:08 +0300

    feat: add serilog, newtonsoft, swagger configs and jwt to Program.cs

--- server - backend/NutritionTracker.Api/Program.cs
+++ server - backend/NutritionTracker.Api/Program.cs
@@ -1,7 +1,23 @@
+using AutoMapper;
+using Azure.Core;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using NutritionTracker.Api.Configuration;
+using NutritionTracker.Api.Core.Helpers;
 using NutritionTracker.Api.Data;
 using NutritionTracker.Api.Repositories;
+using NutritionTracker.Api.Services;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace NutritionTracker.Api
 {
@@ -9,44 +25,141 @@ namespace NutritionTracker.Api
     {
         public static void Main(string[] args)
         {
+            // This initializes a WebApplicationBuilder, which helps you configure services, middleware, and settings for your web application
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            //Configuration to set up Entity Framework Core with SQL Server using a connection string
+            // Register Entity Framework Core with SQL Server context with DI using config connection string
             var connString = builder.Configuration.GetConnectionString("DefaultConnection");  // ConnString is pulled from User-Secrets for now
-            builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(connString));  // AddDbContext is scoped - per request, a new instance is created.
+            builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(connString));  // AddDbContext is scoped - per request, a new instance is created
 
             // TODO: configure a way to add db connection string from the .env
             //var connString = Environment.GetEnvironmentVariable("DEFAULT_DB_CONNECTION");
             //Console.WriteLine($"Connection string: {connString}");
 
-            // Automatically registers all AutoMapper profiles found in the same assembly as MapperConfig
+            // AutoMapper setup. Automatically registers all AutoMapper profiles found in the same assembly as MapperConfig
             builder.Services.AddAutoMapper(cfg => { cfg.AddMaps(typeof(MapperConfig)); });
 
             // Add UnitOfWork DI to the scope of IoC container
             builder.Services.AddRepositories();
 
+            // Add AplicationService DI to the scope of IoC container
+            builder.Services.AddScoped<IApplicationService, ApplicationService>();
+
+
+            // Sets up Serilog for structured logging. Reads config from appsettings.json
+            builder.Host.UseSerilog((context, config) =>
+            {
+                config.ReadFrom.Configuration(context.Configuration);
+            });
+
+            
+            // This configures JWT-based authentication using bearer tokens
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var jwtSettings = builder.Configuration.GetSection("Authentication");
+                options.IncludeErrorDetails = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidIssuer = "https://codingfactory.aueb.gr",
+
+                    ValidateAudience = false,
+                    ValidAudience = "https://api.codingfactory.aueb.gr",
+
+                    ValidateLifetime = true, // ensure not expired
+
+                    ValidateIssuerSigningKey = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                    .GetBytes(builder.Configuration["Authentication:SecretKey"]!))
+                };
+            });        
+
+            // Enables Cross-Origin Resource Sharing so front-end clients can communicate with the API.
+            builder.Services.AddCors(options => {
+                options.AddPolicy("AllowAll",  // unrestricted access (good for testing).
+                    b => b.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin());
+            });
+            builder.Services.AddCors(options => {
+                options.AddPolicy("AngularClient",  // restricted to Angular dev server at port 4200.
+                    b => b.WithOrigins("http://localhost:4200") // Assuming Angular runs on localhost:4200
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+            });
+
+            // Makes Newtonsoft the default json serializer, replacing System.Text.Json
+            builder.Services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                });
+
+            // Registers controller support for MVC - style routing.
             builder.Services.AddControllers();
+
+            // Enables endpoint discovery
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            // Configures Swagger UI and security schemes(JWT auth support)
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "School API", Version = "v1" });
+                // Non-nullable reference are properly documented
+                options.SupportNonNullableReferenceTypes();
+                options.OperationFilter<AuthorizeOperationFilter>();
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        Scheme = JwtBearerDefaults.AuthenticationScheme,
+                        BearerFormat = "JWT"
+                    });
+            });
 
-            // Configure the HTTP request pipeline.
+            //builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+            // _Add services to the container.
+
+            // Builds the app with all configured services and middleware
+            var app = builder.Build();  // finalizes the DI container setup and constructs the application pipeline
+
+            // _Configure the HTTP request pipeline.
+
+            // In development mode, shows interactive Swagger documentation
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            // Forces HTTPS for all requests
             app.UseHttpsRedirection();
 
+            // Applies CORS rules
+            app.UseCors("AllowAll");
+              
+            // Applies Authentication middleware
+            app.UseAuthentication();
+
+            // Applies Authorization rules
             app.UseAuthorization();
 
-
+            // Maps controller endpoints to route requests.
             app.MapControllers();
 
+            // Starts the app and begins listening for incoming HTTP requests.
             app.Run();
         }
     }
