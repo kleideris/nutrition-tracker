f478d0bff16e8faf90d3f74b630602f834c2e98d
Ch. Kleideris <kleiderisch@gmail.com>
2025-08-01 21:08:09 +0300

    feat: add mapping from UserRegisterDTO to user with hashed password

--- server - backend/NutritionTracker.Api/Configuration/MapperConfig.cs
+++ server - backend/NutritionTracker.Api/Configuration/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using NutritionTracker.Api.Data;
 using NutritionTracker.Api.DTO;
+using NutritionTracker.Api.Security;
 
 namespace NutritionTracker.Api.Configuration
 {
@@ -11,8 +12,9 @@ namespace NutritionTracker.Api.Configuration
             CreateMap<UserProfile, UserProfileDTO>();
             CreateMap<UserProfileDTO, UserProfile>()
                 .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null)); // Ignores nulls to protect existing values.
-
             CreateMap<User, UserReadOnlyDTO>().ReverseMap();
+            CreateMap<UserRegisterDTO, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => EncryptionUtil.EncryptPassword(src.Password!)));
 
         }
     }
