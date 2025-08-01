8f9eb38131cd945367ef15b22d9109c84c31a9f0
Ch. Kleideris <kleiderisch@gmail.com>
2025-08-01 21:06:58 +0300

    feat: implement the rest of the userService methods

--- server - backend/NutritionTracker.Api/Services/UserService.cs
+++ server - backend/NutritionTracker.Api/Services/UserService.cs
@@ -1,13 +1,16 @@
 ﻿using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using NutritionTracker.Api.Core.Enums;
 using NutritionTracker.Api.Core.Filters;
 using NutritionTracker.Api.Core.Helpers;
 using NutritionTracker.Api.Data;
 using NutritionTracker.Api.DTO;
-using NutritionTracker.Api.Exceptions;
 using NutritionTracker.Api.Repositories;
 using Serilog;
-using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq.Expressions;
+using System.Security.Claims;
+using System.Text;
 
 namespace NutritionTracker.Api.Services
 {
@@ -17,13 +20,16 @@ namespace NutritionTracker.Api.Services
         private readonly IMapper _mapper;
         private readonly ILogger<UserService> _logger;
 
-        public UserService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<UserService> logger)
+        public UserService(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
-            _logger = new LoggerFactory().AddSerilog().CreateLogger<UserService>();  // create the Logger for user service using a factory method
+            _logger = new LoggerFactory().AddSerilog().CreateLogger<UserService>();  // creates the Logger for user service using a factory method
         }
 
+
+
+        //Finished
         public async Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize, UserFiltersDTO userFilterDTO)
         {
             List<User> users;
@@ -33,7 +39,7 @@ namespace NutritionTracker.Api.Services
             {
                 predicates = UserPredicateBuilder.BuildPredicates(userFilterDTO);
 
-                users = await _unitOfWork.userRepository
+                users = await _unitOfWork.UserRepository
                     .GetAllUsersFilteredPaginatedAsync(pageNumber, pageSize, predicates);
             }
             catch (Exception e)
@@ -44,20 +50,97 @@ namespace NutritionTracker.Api.Services
             return users;
         }
 
-        public Task<User?> GetUserByIdAsync(int id)
+
+        //Finished
+        public async Task<User?> GetUserByIdAsync(int id) => await _unitOfWork.UserRepository.GetAsync(id);
+
+
+        //Finished
+        public async Task<User?> GetUserByUsernameAsync(string username) => await _unitOfWork.UserRepository.GetUserByUsernameAsync(username);
+
+
+        //Finished
+        public async Task<User?> VerifyAndGetUserAsync(UserLoginDTO credentials)
         {
-            throw new NotImplementedException();
+            User? user;
+
+            try
+            {
+                user = await _unitOfWork.UserRepository.AuthenticateUserAsync(credentials.UsernameOrEmail!, credentials.Password!);
+                _logger.LogInformation("{Message}", $"User: " + user + " found and returned.");  // TODO: needs user ToString for this to work
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("{Message}{Exception}", ex.Message, ex.StackTrace);
+                throw;
+            }
+            return user;
         }
 
-        public Task<User?> GetUserByUsernameAsync(string username)
+
+        //Finished
+        public string CreateUserToken(int userId, string username, string email,
+                UserRole userRole, string appSecurityKey)
         {
-            throw new NotImplementedException();
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSecurityKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsInfo = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.Role, userRole.ToString()!)
+            };
+
+            var jwtSecurityToken = new JwtSecurityToken("https://codingfactory.aueb.gr", "https://api.codingfactory.aueb.gr", claimsInfo, DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(3), signingCredentials);
+            //var jwtSecurityToken = new JwtSecurityToken(null, null, claimsInfo, DateTime.UtcNow,
+            //    DateTime.UtcNow.AddHours(3), signingCredentials);
+
+            // Serialize the token
+            var userToken = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return userToken;
         }
 
-        public Task<User?> VerifyAndGetUserAsync(UserLoginDTO credentials)
+
+        //Finished--
+        public async Task<RegisterResult> RegisterUserAsync(UserRegisterDTO dto)
         {
-            throw new NotImplementedException();
-        }                
+            var existingUser = await _unitOfWork.UserRepository.GetUserByUsernameAsync(dto.Username!);
+            if (existingUser != null)
+            {
+                return new RegisterResult { Success = false, ErrorMessage = "Username already in use." };
+            }
+
+            var user = _mapper.Map<User>(dto);
+
+            await _unitOfWork.UserRepository.AddAsync(user);
+            await _unitOfWork.SaveAsync();
+
+            return new RegisterResult { Success = true, UserId = user.Id };
+        }
+
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            bool userDeleted = false;
+
+            try
+            {
+                userDeleted = await _unitOfWork.UserRepository.DeleteAsync(id);
+                _logger.LogInformation("{Message}", "User with id: " + id + " deleted.");
+                await _unitOfWork.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("{Message}{Exception}", ex.Message, ex.StackTrace);
+                throw;
+            }
+
+            return userDeleted;
+        }
     }
 }
 
