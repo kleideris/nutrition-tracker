7e8115161ed0408fb4c33fef25ef33f5680d98c1
Ch. Kleideris <kleiderisch@gmail.com>
2025-08-04 16:43:46 +0300

    feat(frondend): implement .env reading, tailwindcss, concurrently as a 2nd method to run the app

--- client-frontend/vite.config.ts
+++ client-frontend/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+
+console.log("Using port:", process.env.PORT);
+
+// Load environment variables from .env file
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
+  server: {
+    open: true, // This opens the browser automatically
+    port: parseInt(process.env.PORT ?? '5173'), // This sets the frontend port from the .env
+    // proxy: {
+    //   '/api': {
+    //   target: 'http://localhost:5000',
+    //   changeOrigin: true,
+    //   secure: false,
+    //   }
+    // }
+  }
 })
