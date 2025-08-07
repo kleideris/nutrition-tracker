18f094ee4f76620d5ec17932513134c4a14847ab
Ch. Kleideris <kleiderisch@gmail.com>
2025-08-07 23:38:18 +0300

    feat(backend): add MealFoodItemService interface and implementation

--- server-backend/NutritionTracker.Api/Services/IMealFoodItemService.cs
+++ server-backend/NutritionTracker.Api/Services/IMealFoodItemService.cs
@@ -1,6 +1,12 @@
-﻿namespace NutritionTracker.Api.Services
+﻿using NutritionTracker.Api.Data;
+
+namespace NutritionTracker.Api.Services
 {
     public interface IMealFoodItemService
     {
+        Task<MealFoodItem?> GetByJoinedIdsAsync(int mealId, int foodItemId);
+        Task<bool> AddFoodItemToMealAsync(int mealId, int foodItemId, float quantity, string unit);
+        Task<bool> DeleteFoodItemOfMealAsync(int mealId, int foodItemId, float quantity, string unit);
+        Task<bool> UpdateQuantityOfFoodItemAsync(int mealId, int foodItemId, float quantity, string unit);
     }
 }
