2bdf9f2348d9391eafdc3474214369ec0621ec16
Ch. Kleideris <kleiderisch@gmail.com>
2025-08-12 20:08:23 +0300

    feat(frontend): add and refactor routes

--- client-frontend/src/App.tsx
+++ client-frontend/src/App.tsx
@@ -1,20 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 // import RegisterPage from './pages/RegisterPage';
-// import DashboardPage from './pages/DashboardPage';
 // import LogMealPage from './pages/LogMealPage';
+import { AuthProvider } from './context/AuthProvider';
+import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
+import HomePage from './pages/HomePage';
 
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        {/* <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/log" element={<LogMealPage />} /> */}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<HomePage/>} />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path="/dashboard" element={<ProtectedRoute />} />
+            {/* <Route path="/register" element={<RegisterPage />} /> */}
+            {/* <Route path="/log" element={<LogMealPage />} /> */}
+          </Route>
+          
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
